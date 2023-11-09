67b787febd1600a3f389aa2be0939d84945f31e2
liuyuancheng <tenstone@foxmail.com>
2023-11-09 13:37:24 +0800

    chore: 关闭ssl

--- examples/basic/vite.config.ts
+++ examples/basic/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import { GlobSync } from 'glob'
 import { resolve } from 'path'
 import fs from 'fs-extra'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+// import basicSsl from '@vitejs/plugin-basic-ssl'
 
 const getWorkspaceAlias = () => {
   const basePath = resolve(__dirname, '../../')
@@ -29,7 +29,10 @@ const getWorkspaceAlias = () => {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [
+    react(),
+    // basicSsl()
+  ],
   resolve: {
     alias: [
       { find: /^~/, replacement: '' },
